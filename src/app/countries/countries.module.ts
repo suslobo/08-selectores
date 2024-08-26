322ed606b1813f037c4bd9176a915667a5188eb5
slobo <suslobo@hotmail.es>
2024-08-26 13:26:01 +0200

    Formulario reactivo del primer selector

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CountriesRoutingModule } from './countries-routing.module';
 
@@ -13,6 +14,7 @@ import { CountriesRoutingModule } from './countries-routing.module';
   imports: [
     CommonModule,
     CountriesRoutingModule,
+    ReactiveFormsModule, //formulario reactivo
   ]
 })
 export class CountriesModule { }
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SelectorPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required ],
+    country: ['', Validators.required ],
+    borders: ['', Validators.required ],
+  })
+  //hay que enlazar el myForm con el html
+
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
 
 }
